bdbb2c50202dc1e524ccf9570952a9c7a4536a0a
Jeswin <jeswinpk@agilehead.com>
2020-04-02 20:25:25 +0530

    Working native libs after splitting up projects

--- test-in-emulator.sh
+++ test-in-emulator.sh
@@ -3,11 +3,11 @@ cp -r ui/dist www
 
 rm -rf gitstaCoreApiBridge/plugin/libs
 
-# mkdir -p gitstaCoreApiBridge/plugin/libs/armeabi
-# cp ../gitsta-coreapi/target/armv7-linux-androideabi/release/*.so gitstaCoreApiBridge/plugin/libs/armeabi
+# mkdir -p gitstaCoreApiBridge/plugin/libs/armeabi-v7a
+# cp ../gitsta-coreapi/target/armv7-linux-androideabi/release/*.so gitstaCoreApiBridge/plugin/libs/armeabi-v7a
 
-# mkdir -p gitstaCoreApiBridge/plugin/libs/aarch64
-# cp ../gitsta-coreapi/target/aarch64-linux-android/release/*.so gitstaCoreApiBridge/plugin/libs/aarch64
+# mkdir -p gitstaCoreApiBridge/plugin/libs/arm64-v8a
+# cp ../gitsta-coreapi/target/aarch64-linux-android/release/*.so gitstaCoreApiBridge/plugin/libs/arm64-v8a
 
 # mkdir -p gitstaCoreApiBridge/plugin/libs/x86
 # cp ../gitsta-coreapi/target/i686-linux-android/release/*.so gitstaCoreApiBridge/plugin/libs/x86
